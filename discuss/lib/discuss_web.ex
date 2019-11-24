31e20c24bb4f263f947be904345095b30fb9ad63
snaphuman <fhernandezn@gmail.com>
2019-11-24 07:45:23 -0500

    insert topic changeset to database

--- discuss/lib/discuss_web.ex
+++ discuss/lib/discuss_web.ex
@@ -23,6 +23,8 @@ defmodule DiscussWeb do
 
       import Plug.Conn
       import DiscussWeb.Gettext
+
+      alias Discuss.Repo
       alias DiscussWeb.Router.Helpers, as: Routes
     end
   end
--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -11,5 +11,11 @@ defmodule DiscussWeb.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} -> IO.inspect(changeset)
+    end
   end
 end
