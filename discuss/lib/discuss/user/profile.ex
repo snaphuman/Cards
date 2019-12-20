d7f95b55990a855fb54e95b32071a7ce493b8143
snaphuman <fhernandezn@gmail.com>
2019-12-20 08:45:06 -0500

    initialize user profile model and migration

---
+++ discuss/lib/discuss/user/profile.ex
@@ -0,0 +1,12 @@
+defmodule Discuss.User.Profile do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :provider, :string
+    field :token, :string
+
+    timestamps()
+  end
+end
--- discuss/lib/discuss_web/controllers/auth_controller.ex
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -2,12 +2,15 @@ defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
-    IO.puts "+++++"
-    IO.inspect(conn.assigns)
-    IO.puts "+++++"
-    IO.inspect(params)
-    IO.puts "+++++"
-  end
+  alias Discuss.User.Profile
 
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider} = params) do
+
+    user_params = %{token: auth.credentials.token,
+                    email: auth.info.email,
+                    provider: provider}
+
+    changeset = Profile.changeset(%Profile{}, user_params)
+
+  end
 end
