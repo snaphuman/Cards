798f051f1123620516ef83c8df9ce708f4328d41
snaphuman <fhernandezn@gmail.com>
2019-12-23 06:40:41 -0500

    define plug to check the topic owner and hide links from unauthorized users

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -6,6 +6,9 @@ defmodule DiscussWeb.TopicController do
   plug Discuss.Plugs.RequireAuth when action in
   [:new, :create, :edit, :update, :delete]
 
+  plug :check_topic_owner when action in
+  [:update, :edit, :delete]
+
   def index(conn, _params) do
     topics = Repo.all(Topic)
 
@@ -65,6 +68,20 @@ defmodule DiscussWeb.TopicController do
     conn
     |> put_flash(:info, "Topic deleted")
     |> redirect(to: Routes.topic_path(conn, :index))
+
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic, topic_id).profile_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You cannot edit that")
+      |> redirect(to: Routes.topic_path(conn, :index))
+      |> halt()
+    end
 
   end
 end
