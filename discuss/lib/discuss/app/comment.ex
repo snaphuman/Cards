28866ada22f884d82292ede8126c0b0ee22c10ed
snaphuman <fhernandezn@gmail.com>
2019-12-27 12:53:21 -0500

    insert comment with related user id

--- discuss/lib/discuss/app/comment.ex
+++ discuss/lib/discuss/app/comment.ex
@@ -2,12 +2,14 @@ defmodule Discuss.App.Comment do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Discuss.App.{User, Topic}
+
   @derive {Jason.Encoder, only: [:content]}
 
   schema "comments" do
     field :content, :string
-    belongs_to :user, Discuss.App.User
-    belongs_to :topic, Discuss.App.Topic
+    belongs_to :user, User
+    belongs_to :topic, Topic
 
     timestamps();
   end
