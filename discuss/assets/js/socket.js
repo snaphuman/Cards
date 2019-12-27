a757da7702a8529fa3b875beb3e9cd850e64f136
snaphuman <fhernandezn@gmail.com>
2019-12-27 05:13:18 -0500

    ensure json serialization for comments plus rendering at join to channel

--- discuss/assets/js/socket.js
+++ discuss/assets/js/socket.js
@@ -8,7 +8,9 @@ const createSocket = (topicId) => {
   // Now that you are connected, you can join channels with a topic:
   let channel = socket.channel(`comments:${topicId}`, {})
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("ok", resp => {
+      renderComments(resp.comments)
+    })
     .receive("error", resp => { console.log("Unable to join", resp) })
 
   document.querySelector("button").addEventListener("click", function(){
@@ -17,6 +19,18 @@ const createSocket = (topicId) => {
     channel.push('comment:add', { content: content});
 
   })
+}
+
+function renderComments(comments) {
+  const renderedComments = comments.map(comment => {
+    return `
+      <li class="collection-item">
+        ${comment.content}
+      </li>
+      `;
+  });
+
+  document.querySelector(".collection").innerHTML = renderedComments.join("")
 }
 
 window.createSocket = createSocket;
